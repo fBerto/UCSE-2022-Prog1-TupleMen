866eb2459c1a97b082f0f0a107a17e11f50549ce
Franco Bertoni <4francobertoni@gmail.com>
2022-11-01 19:26:37 -0300

    Continuacion de Editar Ingredientes Recetas

---
+++ SuperCook/WindowsFormsApp/IActualizarGrillaIngredientesSeleccionados.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp
+{
+    internal interface IActualzarGrillaIngredientesSeleccionados
+    {
+        void ActualizarGrillaIngredientesSeleccionados();
+        
+    }
+}
